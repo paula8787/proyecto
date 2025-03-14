ea5a9a072651497b2972761d2b11652004c728f4
Paula <echavezp02@gmail.com>
2025-03-14 07:48:23 -0500

    ejercicio en clase MATRICES

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner (System.in);
+        System.out.println("ingresa un numero");
+        int num = input.nextInt();
+        System.out.println("el numero es:"+ num);
+        if (num<=0){
+            System.out.println("numero no valido");
+            input.close();
+            return;
+        }
+        for (int k=1 ; k<=num ; k++) {
+                for (int j=1 ; j<=k ;j++) {
+                    if (j % 2 == 1) {
+                        System.out.print(k);
+                    } else {
+                        System.out.print(k*-1);
+                    }
+                    if (j<k) {
+                            System.out.print("");
+                    }
+                }
+                System.out.println();
+        }
+        input.close();
+    }
+
+}
